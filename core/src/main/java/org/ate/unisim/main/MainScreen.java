ab709f0775fc577e7dad8c4edeeee103fa29a9f9
Mike Vaux <19553364+mikevaux@users.noreply.github.com>
2024-10-25 16:05:45 +0100

    Rough example of multi-viewport, camera enabled main screen

--- core/src/main/java/org/ate/unisim/main/MainScreen.java
+++ core/src/main/java/org/ate/unisim/main/MainScreen.java
@@ -4,13 +4,31 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 import org.ate.unisim.UniSim;
 import org.ate.unisim.menu.MenuScreen;
 
 public class MainScreen implements Screen {
+
+    private static final int WORLD_WIDTH = 32;
+    private static final int WORLD_HEIGHT = 32;
+
     private static MainScreen INSTANCE;
     private final UniSim game;
+
+    OrthographicCamera mapCamera;
+    Viewport mapViewport;
+    Viewport uiViewport;
+
+    BitmapFont font;
+    Sprite mapSprite;
 
     Grid grid;
     BuildingManager buildingManager;
@@ -18,6 +36,19 @@ public class MainScreen implements Screen {
 
     private MainScreen() {
         this.game = UniSim.getInstance();
+
+        mapSprite = new Sprite(new Texture("map.png"));
+        mapSprite.setPosition(0, 0);
+        mapSprite.setSize(WORLD_WIDTH, WORLD_HEIGHT);
+
+        float w = Gdx.graphics.getWidth();
+        float h = Gdx.graphics.getHeight();
+
+        mapCamera = new OrthographicCamera(WORLD_WIDTH, WORLD_HEIGHT * (h/w));
+        mapViewport = new FitViewport(WORLD_WIDTH, WORLD_HEIGHT, mapCamera);
+
+        uiViewport = new ScreenViewport();
+        font = new BitmapFont();
 
         grid = new Grid();
         buildingManager = new BuildingManager();
@@ -48,28 +79,36 @@ public class MainScreen implements Screen {
         if (Gdx.input.isKeyPressed(Input.Keys.ESCAPE)) {
             game.setScreen(new MenuScreen(true));
         }
+
+        mapCamera.update();
     }
+
 
     private void logic(float delta) {
 
     }
 
     private void draw() {
-        ScreenUtils.clear(Color.BLACK);
+        ScreenUtils.clear(Color.OLIVE);
 
-        game.viewport.apply();
-        game.batch.setProjectionMatrix(game.viewport.getCamera().combined);
-
+        mapViewport.apply();
+        game.batch.setProjectionMatrix(mapCamera.combined);
         game.batch.begin();
-        game.font.draw(game.batch, "UniSim!", game.viewport.getWorldWidth()/3, game.viewport.getWorldHeight()/2 + 2);
-        game.font.draw(game.batch, "Press Esc for Main Menu", game.viewport.getWorldWidth()/3, game.viewport.getWorldHeight()/2);
-        game.font.draw(game.batch, "Remaining Time: " + tracker.displayRemainingTime(), game.viewport.getWorldWidth()/3, game.viewport.getWorldHeight()/2 - 2);
+        mapSprite.draw(game.batch);
+        game.batch.end();
+
+        uiViewport.apply();
+        game.batch.setProjectionMatrix(uiViewport.getCamera().combined);
+        game.batch.begin();
+        font.draw(game.batch, "Remaining Time:\n" + tracker.displayRemainingTime(), 5, 42);
         game.batch.end();
     }
 
     @Override
     public void resize(int width, int height) {
-        game.viewport.update(width, height, true);
+        mapViewport.update(width, height, true);
+        mapCamera.position.set(mapCamera.viewportWidth/2,mapCamera.viewportHeight/2,0);
+        uiViewport.update(width, height, true);
     }
 
     @Override

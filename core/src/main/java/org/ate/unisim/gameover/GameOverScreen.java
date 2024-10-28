219dde32cd010cc2aa26bddd100366eb8dff9294
Mike Vaux <19553364+mikevaux@users.noreply.github.com>
2024-10-28 10:13:11 +0000

    Game Over handling, with placeholder Game Over screen

---
+++ core/src/main/java/org/ate/unisim/gameover/GameOverScreen.java
@@ -0,0 +1,59 @@
+package org.ate.unisim.gameover;
+
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import org.ate.unisim.UniSim;
+
+public class GameOverScreen implements Screen {
+    private final UniSim game;
+    Viewport viewport;
+
+    public GameOverScreen() {
+        game = UniSim.getInstance();
+        viewport = new ScreenViewport();
+        viewport.apply();
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+        ScreenUtils.clear(Color.BLACK);
+
+        game.batch.setProjectionMatrix(game.viewport.getCamera().combined);
+        game.batch.begin();
+        game.font.draw(game.batch, "GAME OVER", game.viewport.getWorldWidth()/3, game.viewport.getWorldHeight()/2 + 2);
+        game.batch.end();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        viewport.update(width, height, true);
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}

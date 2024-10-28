1048243413feac3c6dd99e7b009d591c08ee157f
Mike Vaux <19553364+mikevaux@users.noreply.github.com>
2024-10-28 13:35:17 +0000

    Implementation of tmx map, including pan edge bounding

--- core/src/main/java/org/ate/unisim/main/MainScreen.java
+++ core/src/main/java/org/ate/unisim/main/MainScreen.java
@@ -8,8 +8,15 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.ScreenUtils;
-import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.ExtendViewport;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import org.ate.unisim.UniSim;
@@ -18,8 +25,10 @@ import org.ate.unisim.menu.MenuScreen;
 
 public class MainScreen implements Screen {
 
-    private static final int WORLD_WIDTH = 32;
-    private static final int WORLD_HEIGHT = 32;
+    private static final int WORLD_WIDTH = 80;
+    private static final int WORLD_HEIGHT = 60;
+    private static final int TILE_WIDTH = 32;
+    private static final int TILE_HEIGHT = 32;
 
     private static MainScreen INSTANCE;
     private final UniSim game;
@@ -31,29 +40,31 @@ public class MainScreen implements Screen {
     BitmapFont font;
     Sprite mapSprite;
 
+    TiledMap tiledMap;
+    TiledMapRenderer tiledMapRenderer;
+    TiledMapTileLayer layer;
+
     Grid grid;
     BuildingManager buildingManager;
     GameProgressionTracker tracker;
 
     private MainScreen() {
         this.game = UniSim.getInstance();
-
-        mapSprite = new Sprite(new Texture("map.png"));
-        mapSprite.setPosition(0, 0);
-        mapSprite.setSize(WORLD_WIDTH, WORLD_HEIGHT);
-
         float w = Gdx.graphics.getWidth();
         float h = Gdx.graphics.getHeight();
 
-        mapCamera = new OrthographicCamera(WORLD_WIDTH, WORLD_HEIGHT * (h/w));
-        mapViewport = new FitViewport(WORLD_WIDTH, WORLD_HEIGHT, mapCamera);
+        mapCamera = new OrthographicCamera(TILE_WIDTH * 20, TILE_HEIGHT * 20 * (h/w));
+        mapViewport = new ExtendViewport(mapCamera.viewportWidth, mapCamera.viewportHeight, mapCamera);
+
+        tiledMap = new TmxMapLoader().load("map/map.tmx");
+        tiledMapRenderer = new OrthogonalTiledMapRenderer(tiledMap);
+        layer = (TiledMapTileLayer) tiledMap.getLayers().get(0);
 
         uiViewport = new ScreenViewport();
         font = new BitmapFont();
 
         grid = new Grid();
         buildingManager = new BuildingManager();
-
         tracker = new GameProgressionTracker();
     }
 
@@ -87,8 +98,32 @@ public class MainScreen implements Screen {
     private void input(float delta) {
         if (Gdx.input.isKeyPressed(Input.Keys.ESCAPE)) {
             game.setScreen(new MenuScreen(true));
+            return;
         }
 
+        float dist = TILE_WIDTH / 4f;
+        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+            mapCamera.translate(-dist, 0);
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+            mapCamera.translate(dist, 0);
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
+            mapCamera.translate(0, dist);
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
+            mapCamera.translate(0, -dist);
+        }
+
+        float effectiveViewportWidth = mapCamera.viewportWidth * mapCamera.zoom;
+        float effectiveViewportHeight = mapCamera.viewportHeight * mapCamera.zoom;
+        float xMin = effectiveViewportWidth / 2f;
+        float xMax = layer.getWidth() * TILE_WIDTH - effectiveViewportWidth / 2f;
+        float yMin = effectiveViewportHeight / 2f;
+        float yMax = layer.getHeight() * TILE_HEIGHT - effectiveViewportHeight / 2f;
+
+        mapCamera.position.x = MathUtils.clamp(mapCamera.position.x, xMin, xMax);
+        mapCamera.position.y = MathUtils.clamp(mapCamera.position.y, yMin, yMax);
         mapCamera.update();
     }
 
@@ -98,12 +133,15 @@ public class MainScreen implements Screen {
     }
 
     private void draw() {
-        ScreenUtils.clear(Color.OLIVE);
+        ScreenUtils.clear(Color.BLACK);
+
 
         mapViewport.apply();
+
+        tiledMapRenderer.setView(mapCamera);
+        tiledMapRenderer.render();
         game.batch.setProjectionMatrix(mapCamera.combined);
         game.batch.begin();
-        mapSprite.draw(game.batch);
         game.batch.end();
 
         uiViewport.apply();
@@ -117,6 +155,7 @@ public class MainScreen implements Screen {
     public void resize(int width, int height) {
         mapViewport.update(width, height, true);
         mapCamera.position.set(mapCamera.viewportWidth/2,mapCamera.viewportHeight/2,0);
+        mapCamera.update();
         uiViewport.update(width, height, true);
     }
 

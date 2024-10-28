df69dee2d6c70691578f57d79b42da8f9094a259
Mike Vaux <19553364+mikevaux@users.noreply.github.com>
2024-10-28 11:00:19 +0000

    Very basic example map

---
+++ map-tileset.tsx
@@ -0,0 +1,239 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="map-tileset" tilewidth="32" tileheight="32" tilecount="112" columns="8">
+ <image source="map-tileset.png" width="256" height="448"/>
+ <tile id="48">
+  <properties>
+   <property name="buildable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="49">
+  <properties>
+   <property name="buildable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="50">
+  <properties>
+   <property name="buildable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="51">
+  <properties>
+   <property name="buildable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="57">
+  <properties>
+   <property name="buildable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="58">
+  <properties>
+   <property name="buildable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="59">
+  <properties>
+   <property name="buildable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="60">
+  <properties>
+   <property name="buildable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="61">
+  <properties>
+   <property name="buildable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="62">
+  <properties>
+   <property name="buildable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="63">
+  <properties>
+   <property name="buildable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="65">
+  <properties>
+   <property name="buildable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="66">
+  <properties>
+   <property name="buildable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="67">
+  <properties>
+   <property name="buildable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="68">
+  <properties>
+   <property name="buildable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="69">
+  <properties>
+   <property name="buildable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="70">
+  <properties>
+   <property name="buildable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="71">
+  <properties>
+   <property name="buildable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="73">
+  <properties>
+   <property name="buildable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="74">
+  <properties>
+   <property name="buildable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="75">
+  <properties>
+   <property name="buildable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="76">
+  <properties>
+   <property name="buildable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="77">
+  <properties>
+   <property name="buildable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="78">
+  <properties>
+   <property name="buildable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="79">
+  <properties>
+   <property name="buildable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="81">
+  <properties>
+   <property name="buildable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="82">
+  <properties>
+   <property name="buildable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="83">
+  <properties>
+   <property name="buildable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="84">
+  <properties>
+   <property name="buildable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="85">
+  <properties>
+   <property name="buildable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="86">
+  <properties>
+   <property name="buildable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="87">
+  <properties>
+   <property name="buildable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="89">
+  <properties>
+   <property name="buildable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="90">
+  <properties>
+   <property name="buildable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="91">
+  <properties>
+   <property name="buildable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="92">
+  <properties>
+   <property name="buildable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="93">
+  <properties>
+   <property name="buildable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="94">
+  <properties>
+   <property name="buildable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="95">
+  <properties>
+   <property name="buildable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="97">
+  <properties>
+   <property name="buildable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="98">
+  <properties>
+   <property name="buildable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="99">
+  <properties>
+   <property name="buildable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="100">
+  <properties>
+   <property name="buildable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="101">
+  <properties>
+   <property name="buildable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="102">
+  <properties>
+   <property name="buildable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="103">
+  <properties>
+   <property name="buildable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="104">
+  <properties>
+   <property name="buildable" type="bool" value="false"/>
+  </properties>
+ </tile>
+</tileset>

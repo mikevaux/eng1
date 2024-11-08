b65c12f8942cb8e6e4832ad53a5d4385597ed950
Mike Vaux <19553364+mikevaux@users.noreply.github.com>
2024-11-08 10:32:10 +0000

    Remove unused import

--- core/src/main/java/org/ate/unisim/main/MainScreen.java
+++ core/src/main/java/org/ate/unisim/main/MainScreen.java
@@ -9,7 +9,6 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.maps.MapLayers;
 import com.badlogic.gdx.maps.tiled.TiledMap;
